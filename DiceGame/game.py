99e0734dde7e9c671958ccfda7975dc3914f79af
Junnu Danhammer <junnudanhammer@gmail.com>
2024-04-14 20:37:16 +0300

    Update game.py

--- DiceGame/game.py
+++ DiceGame/game.py
@@ -20,59 +20,59 @@ class Player:
         self.attributes.append(attribute)
         self.score += points
         print(
-            f"{Fore.GREEN}{self.name} sai: {attribute} ja {points} pistettä{Style.RESET_ALL}")
+            f"{Fore.GREEN}{self.name} got: {attribute} and {points} points {Style.RESET_ALL}")
 
     def __str__(self):
         attributes_str = ", ".join(self.attributes)
-        return f"{self.name} : {attributes_str}, Pisteet: {self.score}"
+        return f"{self.name} : {attributes_str}, Points: {self.score}"
 
 
 class Game:
     def __init__(self):
-        num_players = int(input("Anna pelaajien määrä: "))
+        num_players = int(input("How many players: "))
         self.players = [
-            Player(input(f"Anna pelaajan {i+1} nimi: ")) for i in range(num_players)]
+            Player(input(f"Give the players {i+1} name: ")) for i in range(num_players)]
         self.dice = [Dice(), Dice()]
         self.attribute_map = {
-            2: ("Yllätyskyykkyä", 5),
-            3: ("Lämmin käsi", 10),
-            4: ("2", 15),
-            5: ("3", 20),
-            6: ("4", 25),
-            7: ("5", 30),
-            8: ("6", 35),
-            9: ("7", 40),
-            10: ("Ylisuuri varvas", 45),
-            11: ("3", 50),
-            12: ("Jäätävä darra", 55)
+            2: ("Suprise Squat", 5),
+            3: ("Harm Hand", 10),
+            4: ("Bees!", 15),
+            5: ("Smashed Potatoes", 20),
+            6: ("Broken Fishrod", 25),
+            7: ("Bigfoot Toe", 30),
+            8: ("Fell Over", 35),
+            9: ("Marika's Lectures", 40),
+            10: ("Alien Attack", 45),
+            11: ("New Porsche", 50),
+            12: ("Yo Mama!", 55)
         }
 
     def roll_dices_and_assign_attribute(self, player):
         input(
-            f"{Fore.YELLOW}Paina Enter heittääksesi noppaa {player.name}...{Style.RESET_ALL}")
+            f"{Fore.YELLOW}Press Enter to throw dice {player.name}...{Style.RESET_ALL}")
         roll_sum = sum(dice.roll() for dice in self.dice)
         attribute_name, points = self.attribute_map.get(
-            roll_sum, ("Ei mitään", 0))
-        print(f"{player.name} heitti {roll_sum} ja sai: {attribute_name}.")
+            roll_sum, ("Nothing", 0))
+        print(f"{player.name} threw {roll_sum} and got: {attribute_name}.")
         player.add_attribute(attribute_name, points)
 
     def play_round(self):
         for player in self.players:
             choice = input(
-                f"{Fore.BLUE}Haluaako {player.name} heittää noppaa? (Joo/Ei) {Style.RESET_ALL}").strip().lower()
-            if choice == 'joo':
+                f"{Fore.BLUE}Does {player.name} want to throw dice? (Yes/No) {Style.RESET_ALL}").strip().lower()
+            if choice == 'yes':
                 self.roll_dices_and_assign_attribute(player)
             else:
                 print(
-                    f"{Fore.RED}{player.name} Meni hoitamaan kanoja ja sai salmonellan silmään. {Style.RESET_ALL}")
+                    f"{Fore.RED}{player.name} Went to take care of the chickens and got salmonella in his eye {Style.RESET_ALL}")
 
     def play(self):
-        print(f"{Fore.CYAN}Peli alkaa! Kukin pelaaja saa kolme yritystä heittää noppaa.\n{Style.RESET_ALL}")
+        print(f"{Fore.CYAN}Game is starting! Each player gets three (3) tries to roll dice.\n{Style.RESET_ALL}")
         for round in range(1, 4):
-            print(f"{Fore.MAGENTA}Kierros {round} alkaa:{Style.RESET_ALL}")
+            print(f"{Fore.MAGENTA}Round {round} starting:{Style.RESET_ALL}")
             self.play_round()
-        print(f"\n{Fore.CYAN}Pelin tulos:{Style.RESET_ALL}")
-        headers = ["Pelaaja", "Ominaisuudet", "Pisteet"]
+        print(f"\n{Fore.CYAN}Games Outcome:{Style.RESET_ALL}")
+        headers = ["Player", "Attributes", "Points"]
         data = [(player.name, ", ".join(player.attributes), player.score)
                 for player in self.players]
         print(tabulate(data, headers=headers, tablefmt="fancy_grid"))
