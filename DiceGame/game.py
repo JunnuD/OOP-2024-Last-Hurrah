142ead777293679ac4378a85d09002166a3df751
Junnu Danhammer <junnudanhammer@gmail.com>
2024-04-14 21:08:53 +0300

    Update game.py

--- DiceGame/game.py
+++ DiceGame/game.py
@@ -1,5 +1,14 @@
-# Object Oriented Programming 2024 - Last Hurrah - Dice Game
-# Authors: Junnu Danhammer & Aarni Kaartokallio
+"""
+Object Oriented Programming 2024 - Last Hurrah - Dice Game
+Authors: Junnu Danhammer & Aarni Kaartokallio
+
+This Python program implements a dice-rolling game designed by amazing authors.
+The game supports multiple players who compete over three rounds to collect points and attributes based on dice rolls.
+Players roll two dice and can earn various attributes with corresponding points based on the sum of the dice.
+Attributes range from 'Alien Attack' to 'Yo Mama- jokes!', with points reflecting the rarity or commonality of the attribute.
+The game utilizes the 'colorama' library for colored terminal text and 'tabulate' for displaying tabulated game outcomes.
+
+"""
 
 import random
 from colorama import Fore, Style, init
