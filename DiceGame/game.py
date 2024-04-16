3c1fc7d7df04abdbc3d25d2ff8323bc0964ab33a
AarniX <aarni86@gmail.com>
2024-04-16 11:04:24 +0300

    sa

--- DiceGame/game.py
+++ DiceGame/game.py
@@ -5,7 +5,7 @@ Authors: Junnu Danhammer & Aarni Kaartokallio
 This Python program implements a dice-rolling game designed by amazing authors.
 The game supports multiple players who compete over three rounds to collect points and attributes based on dice rolls.
 Players roll two dice and can earn various attributes with corresponding points based on the sum of the dice.
-Attributes range from 'Alien Attack' to 'Yo Mama- jokes!', with points reflecting the rarity or commonality of the attribute.
+Attributes range from 'Alien Attack' to 'Yo Mama- jokes! (Magic Dice gives extended attributes)', with points reflecting the rarity or commonality of the attribute.
 The game utilizes the 'colorama' library for colored terminal text and 'tabulate' for displaying tabulated game outcomes.
 
 """
